20fd9ec852c8edcff79946f6529b20679bc59bd8
Sergio Guijarro <sergibirisoeee@gmail.com>
2026-08-14 20:32:34 +0200

    Site updated: 2026-08-14 20:32:33

--- write-ups/data.js
+++ write-ups/data.js
@@ -15,10 +15,10 @@ const MACHINES = [
         badge: "OSCP",
         platform: "htb",
         description: "Anonymous SMB leak LDAP binary with valid credentials, connect with evil-winrm and privilage escalation abusing Resource-Based Constrained Delegation (RBCD)",
-        tags: ["dig", "zone transfer", "kerbrute", "naming context", "smb", "smbclient", "smbmap", "netexec", "file", "wireshark", "ldap", "strings", "objdump", "ghidra", "dnspy", "reverse engineeing", "python", "C#", "ldapsearch", "ldapdomaindump", "kerberoasting", "AS-REP Roasting", "Apache Directory Studio", "Password Spraying", "winrm", "evil-winrm", "bloodhound", "bloodhound.py", "rbcd abuse", "powermard", "powerview", "ps-exec", "TGT"],
+        tags: ["dig", "zone transfer", "kerbrute", "smb", "smbclient", "smbmap", "netexec", "file", "wireshark", "ldap", "strings", "objdump", "ghidra", "dnspy", "reverse engineeing", "python", "C#", "ldapsearch", "ldapdomaindump", "kerberoasting", "AS-REP Roasting", "Apache Directory Studio", "Password Spraying", "winrm", "evil-winrm", "bloodhound", "bloodhound-python", "rbcd", "powermard", "ps-exec", "TGT", "TGS"],
         difficulty: "Easy",              // Easy | Medium | Hard | Insane
-        date: "",
-        youtube: ""
+        date: "14/08/2026",
+        youtube: "https://youtu.be/3MxI5SbvbQY"
         // color: "pink"  <- opcional, si no lo pones, va rotando solo
     }
 ];
