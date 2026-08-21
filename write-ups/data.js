f26af1340c26cd529a9b16f16154f24f5376b456
Sergio Guijarro <sergibirisoeee@gmail.com>
2026-08-22 00:49:10 +0200

    Site updated: 2026-08-22 00:49:09

--- write-ups/data.js
+++ write-ups/data.js
@@ -15,10 +15,21 @@ const MACHINES = [
         badge: "OSCP",
         platform: "htb",
         description: "Anonymous SMB leak LDAP binary with valid credentials, connect with evil-winrm and privilage escalation abusing Resource-Based Constrained Delegation (RBCD)",
-        tags: ["dig", "zone transfer", "kerbrute", "smb", "smbclient", "smbmap", "netexec", "file", "wireshark", "ldap", "strings", "objdump", "ghidra", "dnspy", "reverse engineeing", "python", "C#", "ldapsearch", "ldapdomaindump", "kerberoasting", "AS-REP Roasting", "Apache Directory Studio", "Password Spraying", "winrm", "evil-winrm", "bloodhound", "bloodhound-python", "rbcd", "powermard", "ps-exec", "TGT", "TGS"],
+        tags: ["dig", "zone transfer", "kerbrute", "smb", "smbclient", "smbmap", "netexec", "file", "wireshark", "ldap", "strings", "objdump", "ghidra", "dnspy", "reverse engineeing", "python scripting", "C# scripting", "ldapsearch", "ldapdomaindump", "kerberoasting", "AS-REP Roasting", "apache directory studio", "password spraying", "winrm", "evil-winrm", "bloodhound", "bloodhound-python", "rbcd", "powermard", "ps-exec", "TGT", "TGS"],
         difficulty: "Easy",              // Easy | Medium | Hard | Insane
         date: "14/08/2026",
         youtube: "https://youtu.be/3MxI5SbvbQY"
         // color: "pink"  <- opcional, si no lo pones, va rotando solo
-    }
+    },
+	{
+		title: "SolidState",
+		badge: "OSCP",
+		platform: "htb",
+		description: "James SMTP vulnerabilty allow to read email of users leaking ssh password. Then, privilege escalation abusing cron tab",
+		tags: ["ssh user enum", "James SMTP", "searchsploit", "telnet", "smtp", "pop3", "ssh", "rbash", "abuse cron tab", "proc-scan", "bash scripting"],
+		difficulty: "Medium",              // Easy | Medium | Hard | Insane
+		date: "",
+		youtube: ""
+		// color: "pink"  <- opcional, si no lo pones, va rotando solo
+	},
 ];
